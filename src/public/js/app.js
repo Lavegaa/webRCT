ae1b66c16cd8191f87e3fa2d93086853d7fa35d2
lavegaa <cksgh405@naver.com>
2021-11-09 00:17:48 +0900

    video 선택

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,87 @@
 const socket = io();
+
+const myFace = document.getElementById("myFace");
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+const muteButton = document.getElementById("mute");
+const cameraButton = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
+
+const getCameras = async () => {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoInput");
+    const currentCamera = myStream.getVideoTracks()[0];
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
+      camerasSelect.appendChild(option);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getMedia = async (deviceId) => {
+  try {
+    const initConstrains = {
+      audio: true,
+      video: true,
+    };
+    const cameraConstrains = {
+      audio: true,
+      video: { deviceId: { exact: deviceId } },
+    };
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? initConstrains : cameraConstrains
+    );
+    console.log(myStream);
+    myFace.srcObject = myStream;
+    if (!deviceId) {
+      await getCameras();
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+getMedia();
+
+const handleMuteClick = () => {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (!muted) {
+    muteButton.innerText = "Unmete";
+    muted = true;
+  } else {
+    muteButton.innerText = "Mute";
+    muted = false;
+  }
+};
+const handleCameraClick = () => {
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (!cameraOff) {
+    cameraButton.innerText = "Turn Camera Off";
+    cameraOff = true;
+  } else {
+    cameraButton.innerText = "Turn Camera On";
+    cameraOff = false;
+  }
+};
+
+const handleCamerasSelect = async () => {
+  await getMedia(camerasSelect.value);
+};
+
+muteButton.addEventListener("click", handleMuteClick);
+cameraButton.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCamerasSelect);
